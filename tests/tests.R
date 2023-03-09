shikokuchuo/mirai

commit 51f2f80c360d872437fd9a17def3ae20184edf79

re-work server timers and tasks logic

DESCRIPTION
@@ -1,7 +1,7 @@
 Package: mirai
 Type: Package
 Title: Minimalist Async Evaluation Framework for R
-Version: 0.7.2.9028
+Version: 0.7.2.9029
 Description: Lightweight parallel code execution, local or distributed across
     the network. Designed for simplicity, a 'mirai' evaluates an arbitrary
     expression asynchronously, resolving automatically upon completion.

R/mirai.R
@@ -27,24 +27,28 @@
 #'     'tcp://192.168.0.2:5555' or 'ws://192.168.0.2:5555/path'.
 #' @param nodes [default NULL] if supplied, this server instance will run as an
 #'     active queue (task scheduler) with the specified number of nodes.
-#' @param idletime [default Inf] maximum idle time, since completion of the last
-#'     task (in milliseconds) before exiting.
-#' @param walltime [default Inf] soft walltime, or the minimum amount of real
-#'     time taken (in milliseconds) before exiting.
-#' @param tasklimit [default Inf] the maximum number of tasks to execute (task
-#'     limit) before exiting.
-#' @param asyncdial [default TRUE] (for debugging purposes) whether to dial in
-#'     to the client asynchronously. An asynchronous dial is more resilient and
-#'     will continue retrying if not immediately successful, however this can
-#'     mask potential connection issues. If FALSE, will error if a connection is
-#'     not immediately possible (e.g. \code{\link{daemons}} has yet to be called
-#'     on the client, or the specified port is not open etc.).
+#' @param asyncdial [default TRUE] whether to dial in to the client
+#'     asynchronously. An asynchronous dial is more resilient and will continue
+#'     retrying if not immediately successful. However this can mask potential
+#'     connection issues and specifying FALSE is useful for debugging purposes,
+#'     producing an error if a connection is not immediately possible (e.g.
+#'     \code{\link{daemons}} has yet to be called on the client, or the
+#'     specified port is not open etc.).
+#' @param maxtasks [default Inf] (applicable to severs/nodes) the maximum number
+#'     of tasks to execute (task limit) before exiting.
+#' @param idletime [default Inf] (applicable to severs/nodes) maximum idle time,
+#'     since completion of the last task (in milliseconds) before exiting.
+#' @param walltime [default Inf] (applicable to severs/nodes) soft walltime, or
+#'     the minimum amount of real time taken (in milliseconds) before exiting.
+#' @param timerstart [default 0L] (applicable to severs/nodes) number of
+#'     completed tasks after which to start the timer for 'idletime' and
+#'     'walltime'. 0L implies timers are started upon server launch.
 #' @param ... reserved but not currently used.
-#' @param pollfreqh [default 5L] applicable for an active queue only, the high
+#' @param pollfreqh [default 5L] (applicable to active queues only) the high
 #'     polling frequency for the queue in milliseconds (used when there are
 #'     active tasks). Setting a lower value will be more responsive but at the
 #'     cost of consuming more resources on the queue thread.
-#' @param pollfreql [default 50L] applicable for an active queue only, the low
+#' @param pollfreql [default 50L] (applicable to active queues only) the low
 #'     polling frequency for the queue in milliseconds (used when there are no
 #'     active tasks). Setting a lower value will be more responsive but at the
 #'     cost of consuming more resources on the queue thread.
@@ -65,8 +69,9 @@
 #'
 #' @export
 #'
-server <- function(url, nodes = NULL, idletime = Inf, walltime = Inf, tasklimit = Inf,
-                   asyncdial = TRUE, ..., pollfreqh = 5L, pollfreql = 50L) {
+server <- function(url, nodes = NULL, asyncdial = TRUE, maxtasks = Inf,
+                   idletime = Inf, walltime = Inf, timerstart = 0L, ...,
+                   pollfreqh = 5L, pollfreql = 50L) {
 
   sock <- socket(protocol = "rep", dial = url, autostart = if (asyncdial) TRUE else NA)
   devnull <- file(nullfile(), open = "w", blocking = FALSE)
@@ -78,9 +83,6 @@ server <- function(url, nodes = NULL, idletime = Inf, walltime = Inf, tasklimit 
     sink(type = "message")
     close(devnull)
   })
-  count <- 0L
-  idle <- FALSE
-  start <- mclock()
 
   if (is.numeric(nodes)) {
 
@@ -137,7 +139,7 @@ server <- function(url, nodes = NULL, idletime = Inf, walltime = Inf, tasklimit 
 
     suspendInterrupts({
 
-      while (count < tasklimit && mclock() - start < walltime && if (idle) mclock() - idle < idletime else TRUE) {
+      repeat {
 
         activevec <- as.integer(unlist(lapply(servers, stat, "pipes")))
         newcon <- as.logical(pmax.int(activevec - activestore, 0L))
@@ -147,19 +149,13 @@ server <- function(url, nodes = NULL, idletime = Inf, walltime = Inf, tasklimit 
 
         active <- sum(activevec)
         free <- which(serverfree & activevec)
-        if (length(free) == active) {
-          if (active && !idle) idle <- mclock()
-          msleep(pollfreql)
-        } else {
-          if (idle) idle <- FALSE
-          msleep(pollfreqh)
-        }
+        msleep(if (length(free) == active) pollfreql else pollfreqh)
 
         ctrchannel && !unresolved(controlq) && {
           data <- `attributes<-`(c(activevec, assigned - complete, assigned, complete),
                                  list(dim = c(nodes, 4L),
                                       dimnames = list(servernames,
-                                                      c("status_active", "status_busy", "tasks_assigned", "tasks_complete"))))
+                                                      c("status_online", "status_busy", "tasks_assigned", "tasks_complete"))))
           send(sockc, data = data, mode = 1L)
           controlq <- recv_aio(sockc, mode = 5L)
           next
@@ -192,7 +188,6 @@ server <- function(url, nodes = NULL, idletime = Inf, walltime = Inf, tasklimit 
             q <- queue[[i]][["daemon"]]
             serverfree[q] <- TRUE
             complete[q] <- complete[q] + 1L
-            count <- count + 1L
             ctx <- context(sock)
             req <- recv_aio(ctx, mode = 1L)
             queue[[i]] <- list(ctx = ctx, req = req)
@@ -202,15 +197,19 @@ server <- function(url, nodes = NULL, idletime = Inf, walltime = Inf, tasklimit 
     })
 
   } else {
+
+    count <- 0L
+    start <- mclock()
     if (idletime == Inf) idletime <- NULL
-    while (count < tasklimit && mclock() - start < walltime) {
+    while (count < maxtasks && mclock() - start < walltime) {
 
       ctx <- context(sock)
-      envir <- recv(ctx, mode = 1L, block = idletime)
-      is.integer(envir) && break
+      while (is.integer(envir <- recv(ctx, mode = 1L, block = idletime)))
+        count >= timerstart && return(invisible())
       data <- tryCatch(eval(expr = envir[[".expr"]], envir = envir, enclos = NULL),
                        error = mk_mirai_error, interrupt = mk_interrupt_error)
       send(ctx, data = data, mode = 1L)
+      if (count < timerstart) start <- mclock()
       count <- count + 1L
 
     }
@@ -380,9 +379,9 @@ mirai <- function(.expr, ..., .args = list(), .timeout = NULL, .compute = "defau
 #'     \item{\code{connections}} {- number of active connections.}
 #'     \item{\code{daemons}} {- number of daemons, or the client URL when
 #'     running a passive queue.}
-#'     \item{\code{nodes}} {- a matrix of URL, active (connected) and busy
-#'     status, as well as cumulative tasks assigned and completed (reset if a
-#'     node re-connects), or else NA if not running an active queue.}
+#'     \item{\code{nodes}} {- a matrix of URL, online and busy status, as well
+#'     as cumulative tasks assigned and completed (reset if a node re-connects),
+#'     or else NA if not running an active queue.}
 #'     }
 #'
 #' @details Use \code{daemons(0)} to reset all daemon connections at any time.

man/daemons.Rd
@@ -37,9 +37,9 @@ Setting daemons: integer number of daemons set, or the character
     \item{\code{connections}} {- number of active connections.}
     \item{\code{daemons}} {- number of daemons, or the client URL when
     running a passive queue.}
-    \item{\code{nodes}} {- a matrix of URL, active (connected) and busy
-    status, as well as cumulative tasks assigned and completed (reset if a
-    node re-connects), or else NA if not running an active queue.}
+    \item{\code{nodes}} {- a matrix of URL, online and busy status, as well
+    as cumulative tasks assigned and completed (reset if a node re-connects),
+    or else NA if not running an active queue.}
     }
 }
 \description{

man/server.Rd
@@ -7,10 +7,11 @@
 server(
   url,
   nodes = NULL,
+  asyncdial = TRUE,
+  maxtasks = Inf,
   idletime = Inf,
   walltime = Inf,
-  tasklimit = Inf,
-  asyncdial = TRUE,
+  timerstart = 0L,
   ...,
   pollfreqh = 5L,
   pollfreql = 50L
@@ -24,30 +25,35 @@ connect to and (optionally) a path for websocket URLs e.g.
 \item{nodes}{[default NULL] if supplied, this server instance will run as an
 active queue (task scheduler) with the specified number of nodes.}
 
-\item{idletime}{[default Inf] maximum idle time, since completion of the last
-task (in milliseconds) before exiting.}
+\item{asyncdial}{[default TRUE] whether to dial in to the client
+asynchronously. An asynchronous dial is more resilient and will continue
+retrying if not immediately successful. However this can mask potential
+connection issues and specifying FALSE is useful for debugging purposes,
+producing an error if a connection is not immediately possible (e.g.
+\code{\link{daemons}} has yet to be called on the client, or the
+specified port is not open etc.).}
 
-\item{walltime}{[default Inf] soft walltime, or the minimum amount of real
-time taken (in milliseconds) before exiting.}
+\item{maxtasks}{[default Inf] (applicable to severs/nodes) the maximum number
+of tasks to execute (task limit) before exiting.}
 
-\item{tasklimit}{[default Inf] the maximum number of tasks to execute (task
-limit) before exiting.}
+\item{idletime}{[default Inf] (applicable to severs/nodes) maximum idle time,
+since completion of the last task (in milliseconds) before exiting.}
 
-\item{asyncdial}{[default TRUE] (for debugging purposes) whether to dial in
-to the client asynchronously. An asynchronous dial is more resilient and
-will continue retrying if not immediately successful, however this can
-mask potential connection issues. If FALSE, will error if a connection is
-not immediately possible (e.g. \code{\link{daemons}} has yet to be called
-on the client, or the specified port is not open etc.).}
+\item{walltime}{[default Inf] (applicable to severs/nodes) soft walltime, or
+the minimum amount of real time taken (in milliseconds) before exiting.}
+
+\item{timerstart}{[default 0L] (applicable to severs/nodes) number of
+completed tasks after which to start the timer for 'idletime' and
+'walltime'. 0L implies timers are started upon server launch.}
 
 \item{...}{reserved but not currently used.}
 
-\item{pollfreqh}{[default 5L] applicable for an active queue only, the high
+\item{pollfreqh}{[default 5L] (applicable to active queues only) the high
 polling frequency for the queue in milliseconds (used when there are
 active tasks). Setting a lower value will be more responsive but at the
 cost of consuming more resources on the queue thread.}
 
-\item{pollfreql}{[default 50L] applicable for an active queue only, the low
+\item{pollfreql}{[default 50L] (applicable to active queues only) the low
 polling frequency for the queue in milliseconds (used when there are no
 active tasks). Setting a lower value will be more responsive but at the
 cost of consuming more resources on the queue thread.}

commit 83099113b9a955bf6359ee68d55fa71cfdfc1fd1

fixes critical bug introduced in 51f2f80

DESCRIPTION
@@ -1,7 +1,7 @@
 Package: mirai
 Type: Package
 Title: Minimalist Async Evaluation Framework for R
-Version: 0.7.2.9030
+Version: 0.7.2.9031
 Description: Lightweight parallel code execution, local or distributed across
     the network. Designed for simplicity, a 'mirai' evaluates an arbitrary
     expression asynchronously, resolving automatically upon completion.

R/mirai.R
@@ -102,7 +102,7 @@ server <- function(url, nodes = NULL, asyncdial = TRUE, maxtasks = Inf,
 
     if (ctrchannel) {
       sockc <- socket(protocol = "bus", dial = url[2L], autostart = if (asyncdial) TRUE else NA)
-      on.exit(expr = close(sockc), add = TRUE)
+      on.exit(expr = close(sockc), add = TRUE, after = FALSE)
       controlq <- recv_aio(sockc, mode = 5L)
     }
 
@@ -139,7 +139,7 @@ server <- function(url, nodes = NULL, asyncdial = TRUE, maxtasks = Inf,
     on.exit(expr = for (i in seq_nodes) {
       send(servers[[i]], data = .__scm__., mode = 2L)
       close(servers[[i]])
-    }, add = TRUE)
+    }, add = TRUE, after = FALSE)
 
     suspendInterrupts({
 
@@ -212,18 +212,22 @@ server <- function(url, nodes = NULL, asyncdial = TRUE, maxtasks = Inf,
   } else {
 
     if (idletime > walltime) idletime <- walltime else if (idletime == Inf) idletime <- NULL
-    while (count < maxtasks && mclock() - start < walltime) {
+    repeat {
+      while (count < maxtasks && mclock() - start < walltime) {
 
-      ctx <- context(sock)
-      while (is.integer(envir <- recv(ctx, mode = 1L, block = idletime)))
-        count >= timerstart && return(invisible())
-      data <- tryCatch(eval(expr = envir[[".expr"]], envir = envir, enclos = NULL),
-                       error = mk_mirai_error, interrupt = mk_interrupt_error)
-      send(ctx, data = data, mode = 1L)
-      if (count < timerstart) start <- mclock()
-      count <- count + 1L
+        ctx <- context(sock)
+        envir <- recv(ctx, mode = 1L, block = idletime)
+        is.integer(envir) && break
+        data <- tryCatch(eval(expr = envir[[".expr"]], envir = envir, enclos = NULL),
+                         error = mk_mirai_error, interrupt = mk_interrupt_error)
+        send(ctx, data = data, mode = 1L)
+        if (count < timerstart) start <- mclock()
+        count <- count + 1L
 
+      }
+      count < timerstart || break
     }
+
   }
 
   msleep(1900L)

tests/tests.R
@@ -25,6 +25,21 @@ nanotest(is.character(b) || is.character(b$data))
 nanotest(!unresolved(m))
 nanotestn(stop_mirai(m))
 Sys.sleep(2.2)
+nanotest(daemons(1L) == 1L)
+me <- mirai(mirai())
+nanotest(is_mirai_error(call_mirai(me)$data))
+nanotest(!is_mirai_interrupt(me$data))
+nanotest(is_error_value(me[["data"]]))
+df <- data.frame(a = 1, b = 2)
+dm <- mirai(as.matrix(df), .args = list(df), .timeout = 1000L)
+nanotest(is_mirai(call_mirai(dm)))
+nanotest(!unresolved(dm))
+nanotest(is.matrix(dm$data))
+nanotestn(stop_mirai(dm))
+nanotest(daemons()[["daemons"]] == 1L)
+nanotest(daemons(0L) == 0L)
+nanotest(daemons()[["daemons"]] == 0L)
+Sys.sleep(0.1)
 nanotest(daemons(value <- sprintf(mirai:::.urlfmt, runif(1, 1000000, 9999999))) == value)
 Sys.sleep(0.1)
 nanotest(daemons(value <- "ws://:0") != value)
@@ -53,10 +68,9 @@ nanotesterr(daemons(raw(0L)), "numeric, character")
 nanotesterr(server("URL"), "argument")
 nanotest(daemons(0L) == 0L)
 Sys.sleep(1)
+nanotest(is_mirai_error(r <- mirai:::mk_mirai_error(list())))
+me$data
 m
 b
-nanotest(is_mirai_error(r <- mirai:::mk_mirai_error(list())))
-r
-nanotest(is_mirai_interrupt(r <- mirai:::mk_interrupt_error()))
 r
 