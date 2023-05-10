8fde2424dc3b001c60b9a67322e3cf0a72fd7782
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2023-05-10 14:31:14 +0100

    use these tests

--- tests/tests.R
+++ tests/tests.R
@@ -110,9 +110,9 @@ Sys.sleep(0.5)
 nanotestz(daemons(0))
 Sys.sleep(1L)
 
-nanotest(daemons(n = 2, "tcp://127.0.0.1:0") == 2L)
+nanotest(daemons(n = 2, "tcp://:0") == 2L)
 Sys.sleep(1L)
-if (.Platform[["OS.type"]] != "windows") nanotest(is.integer(launch_server(mirai:::..[["default"]][["urls"]][1L], maxtasks = 1L)))
+nanotest(is.integer(launch_server(mirai:::..[["default"]][["urls"]][1L], maxtasks = 1L)))
 Sys.sleep(1L)
 tstatus <- daemons()[["daemons"]]
 nanotest(is.matrix(tstatus) || is_error_value(tstatus))
@@ -123,8 +123,8 @@ if (is.matrix(tstatus)) {
   nanotest(is.character(tparse2 <- nanonext::parse_url(tdn1[2L])))
   nanotest((tport <- as.integer(tparse1[["port"]])) > 0L)
   nanotest(as.integer(tparse2[["port"]]) != tport)
-  nanotest(is.integer(sum(tstatus[, "online"])))
-  nanotest(is.integer(sum(tstatus[, "instance"])))
+  nanotestz(sum(tstatus[, "online"]))
+  nanotestz(sum(tstatus[, "instance"]))
   nanotestz(sum(tstatus[, "assigned"]))
   nanotestz(sum(tstatus[, "complete"]))
 }
