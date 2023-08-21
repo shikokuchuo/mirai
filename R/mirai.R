a58b19e80d4a2a25890f355c45efcaeace4dd733
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2023-08-21 11:49:39 +0100

    optimisations

--- R/mirai.R
+++ R/mirai.R
@@ -750,7 +750,7 @@ daemons <- function(n, url = NULL, dispatcher = TRUE, tls = NULL, ..., .compute 
         urlc <- strcat(urld, "c")
         sock <- req_socket(urld)
         sockc <- req_socket(urlc, resend = 0L)
-        launch_and_sync_daemon(sock = sock, synctime = if (is.numeric(dispatcher)) dispatcher else .timelimit, urld, parse_dots(...), url, n, urlc, tls = tls)
+        launch_and_sync_daemon(sock = sock, synctime = dispatcher + is.logical(dispatcher) * .timelimit, urld, parse_dots(...), url, n, urlc, tls = tls)
         init_monitor(sockc = sockc, envir = envir)
       } else {
         sock <- req_socket(url, tls = if (length(tls)) tls_config(server = tls))
@@ -772,7 +772,7 @@ daemons <- function(n, url = NULL, dispatcher = TRUE, tls = NULL, ..., .compute 
       length(envir[["n"]]) || return(0L)
 
       close(envir[["sock"]])
-      if (length(envir[["sockc"]])) close(envir[["sockc"]])
+      length(envir[["sockc"]]) && close(envir[["sockc"]])
       envir <- NULL
       `[[<-`(.., .compute, new.env(hash = FALSE))
 
@@ -785,7 +785,7 @@ daemons <- function(n, url = NULL, dispatcher = TRUE, tls = NULL, ..., .compute 
       if (dispatcher) {
         urlc <- strcat(urld, "c")
         sockc <- req_socket(urlc, resend = 0L)
-        launch_and_sync_daemon(sock = sock, synctime = if (is.numeric(dispatcher)) dispatcher else .timelimit, urld, dots, n, urlc)
+        launch_and_sync_daemon(sock = sock, synctime = dispatcher + is.logical(dispatcher) * .timelimit, urld, dots, n, urlc)
         init_monitor(sockc = sockc, envir = envir)
       } else {
         for (i in seq_len(n))
