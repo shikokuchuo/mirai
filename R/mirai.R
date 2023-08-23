97ebf5147cb38a896a51047f8af36cf643929dd9
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2023-08-23 21:17:22 +0100

    set libpath by nanonext dynlib

--- R/mirai.R
+++ R/mirai.R
@@ -1279,7 +1279,11 @@ launch_daemon <- function(..., tls = NULL) {
   dots <- list(...)
   dlen <- length(dots)
   output <- dlen > 1L && is.object(dots[[2L]])
-  libpath <- if (dlen > 3L) file.path(attr(suppressMessages(findPackageEnv("package:mirai")), "path"), "..")
+  libpath <- if (dlen > 3L) {
+    dlibs <- .dynLibs()
+    libinfo <- .subset2(dlibs, match("nanonext", as.character(lapply(dlibs, .subset2, 1L))))
+    dirname(dirname(dirname(.subset2(libinfo, "path"))))
+  }
   system2(command = .command, args = c(if (dlen > 3L) "--vanilla", "-e", write_args(dots, tls = tls, libpath = libpath)), stdout = if (output) "", stderr = if (output) "", wait = FALSE)
 }
 
