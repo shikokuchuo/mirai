3c18521dc32de046b4a85e1d13c55e310ee3b199
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2023-09-18 22:51:39 +0100

    no more need for sha1

--- R/mirai-package.R
+++ R/mirai-package.R
@@ -50,8 +50,8 @@
 #'
 #' @importFrom nanonext call_aio .context cv cv_value dial is_error_value listen
 #'     lock mclock msleep opt opt<- parse_url pipe_notify random recv
-#'     recv_aio_signal request request_signal send send_aio sha1 socket stat
-#'     stop_aio strcat tls_config unresolved until wait write_cert
+#'     recv_aio_signal request request_signal send send_aio socket stat stop_aio
+#'     strcat tls_config unresolved until wait write_cert
 #' @importFrom parallel nextRNGStream
 #' @importFrom stats rexp
 #'
