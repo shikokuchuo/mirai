7fb34131f95e279e8b50e33bc6b81b671037628a
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2023-12-02 17:55:05 +0000

    improve status timeout errors

--- R/dispatcher.R
+++ R/dispatcher.R
@@ -312,8 +312,9 @@ get_tls <- function(baseurl, tls, pass) {
 sub_real_port <- function(port, url) sub("(?<=:)0(?![^/])", port, url, perl = TRUE)
 
 query_dispatcher <- function(sock, command, mode) {
-  send(sock, data = command, mode = 2L, block = .timelimit)
-  recv(sock, mode = mode, block = .timelimit)
+  res <- send(sock, data = command, mode = 2L, block = .timelimit)
+  res && return(res)
+  recv(sock, mode = mode, block = .timelimit + .timelimit)
 }
 
 create_req <- function(ctx, cv)
