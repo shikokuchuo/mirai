a9dc3c175e2f6d098b7e6e5eed32c6368cc235fd
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2024-04-19 08:34:57 +0100

    optimize promise callback

--- R/promises.R
+++ R/promises.R
@@ -71,7 +71,7 @@ as.promise.mirai <- function(x) {
     promise <- promises::then(
       promises::promise(
         function(resolve, reject)
-          attr(x, "callback") <- function() resolve(.subset2(x, "data"))
+          attr(x, "callback") <- environment()
       ),
       onFulfilled = function(value)
         if (is_error_value(value) && !is_mirai_interrupt(value))
