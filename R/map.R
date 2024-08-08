53be07838ef16dfaaa8a3ca7d3e2bb2e5c54d3dd
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2024-08-08 10:27:12 +0100

    partially reverts #134 for safety

--- R/map.R
+++ R/map.R
@@ -164,11 +164,11 @@ mirai_map <- function(.x, .f, ..., .args = list(), .promise = NULL, .compute = "
     xilen <- length(.x[[1L]])
     cond <- xilen > 1L && all(as.integer(lapply(.x, length)) == xilen)
   }
-  vec <- if (cond) {
-    if (is.matrix(.x))
-      lapply(
-        seq_len(xilen),
-        function(i) mirai(
+  if (cond) {
+  vec <- vector(mode = "list", length = xilen)
+    if (is.matrix(.x)) {
+      for (i in seq_len(xilen))
+        vec[[i]] <- mirai(
           .expr = do.call(.f, c(as.list(.x), .args)),
           .f = .f,
           .x = .x[i, ],
@@ -176,33 +176,28 @@ mirai_map <- function(.x, .f, ..., .args = list(), .promise = NULL, .compute = "
           .args = list(.args = .args),
           .compute = .compute
         )
-      ) else
-        lapply(
-          seq_len(xilen),
-          function(i) mirai(
-            .expr = do.call(.f, c(.x, .args)),
-            .f = .f,
-            .x = lapply(.x, .subset2, i),
-            ...,
-            .args = list(.args = .args),
-            .compute = .compute
-          )
-        )
+    } else {
+      for (i in seq_len(xilen))
+        vec[[i]] <- mirai(
+        .expr = do.call(.f, c(.x, .args)),
+        .f = .f,
+        .x = lapply(.x, .subset2, i),
+        ...,
+        .args = list(.args = .args),
+        .compute = .compute
+      )
+    }
   } else {
-    `names<-`(
-      lapply(
-        seq_along(.x),
-        function(i) mirai(
-          .expr = do.call(.f, c(list(.x), .args)),
-          .f = .f,
-          .x = .subset2(.x, i),
-          ...,
-          .args = list(.args = .args),
-          .compute = .compute
-        )
-      ),
-      names(.x)
-    )
+    vec <- `names<-`(vector(mode = "list", length = length(.x)), names(.x))
+    for (i in seq_along(vec))
+      vec[[i]] <- mirai(
+        .expr = do.call(.f, c(list(.x), .args)),
+        .f = .f,
+        .x = .subset2(.x, i),
+        ...,
+        .args = list(.args = .args),
+        .compute = .compute
+      )
   }
 
   if (length(.promise))
